793657061ae170e68f61afd26ecd3bd48f69c95d
MariyaSoulimani <soulimanimaria@gmail.com>
2023-03-17 14:28:45 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print the lowercase alphabet in reverse.
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 122; letter >= 97; i--)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
