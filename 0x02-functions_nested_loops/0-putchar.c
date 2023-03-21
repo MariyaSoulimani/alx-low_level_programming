70a007f999e9f38bfd82c60cf9cd882a184809d0
MariyaSoulimani <soulimanimaria@gmail.com>
2023-03-21 09:29:39 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,15 @@
  */
 int main(void)
 {
-char arr[] = "_putchar";
+char arrI[] = "_putchar";
 
 int i;
 
-	for (i = 0; i < 8; ++i)
+	for (i = 0; i < 8; i++)
 	{
 		_putchar (arr[i]);
 	}
+	_putchar ('\n');
 	return (0);
 }
 
