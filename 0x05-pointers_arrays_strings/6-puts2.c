a31116c57d1421c0234ee1d0bc428af630e98e9e
MariyaSoulimani <soulimanimaria@gmail.com>
2023-03-27 18:07:24 -0700

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string
+ * puts2 - prints every other character of a string.
  *
- * @str: string to be printed
+ * @str: string to be printed.
  */
 void puts2(char *str)
 {
