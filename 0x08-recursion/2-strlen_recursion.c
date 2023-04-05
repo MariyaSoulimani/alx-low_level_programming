5609a9899bfdb83479c1b31f6ef49a2aafa2b079
MariyaSoulimani <soulimanimaria@gmail.com>
2023-04-05 14:05:37 -0700

    2. Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,6 +7,11 @@
  */
 int _strlen_recursion(char *s)
 {
+	int len = 0;
+
 	if (*s > '\0')
-		return (_strlen_recursion(s + 1) + 1);
+	{
+		len += _strlen_recursion(s + 1) + 1;
+	}
+	return (len);
 }
