b81898bd0e646d2d7cc8984caf21f03c94c74d0a
MariyaSoulimani <soulimanimaria@gmail.com>
2023-04-09 18:16:17 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,14 +11,13 @@
 int main(int argc, char *argv[])
 {
 	int sum = 0;
-	char *a;
+	char *c;
 
 	while (--argc)
 	{
-		for (a = argv[argc]; *a; a++)
-			if (*a < '0' || *a > '9')
-				printf("Error\n");
-				return (1);
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
 		sum += atoi(argv[argc]);
 	}
 	printf("%d\n", sum);
