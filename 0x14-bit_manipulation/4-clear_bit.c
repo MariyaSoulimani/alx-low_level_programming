3dbb63a21de833af086d99e15a5741ffb41ea860
MariyaSoulimani <soulimanimaria@gmail.com>
2023-05-05 15:08:14 -0700

    4. 100

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,15 +9,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int bit_mask;
-
-	if (index >= 64)
-	{
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
-	}
 
-	bit_mask = 1UL << index;
-	*n &= ~bit_mask;
+	*n &= ~(1 << index);
 
 	return (1);
 }
