bc82624772a9f0e91f98f34830523fdc5b7f4f2d
MariyaSoulimani <soulimanimaria@gmail.com>
2023-06-15 16:50:06 -0700

    2. Add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,9 +15,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!new_node)
 		return (NULL);
 
-	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
+	new_node->n = n;
 
 	if (!*head)
 	{
